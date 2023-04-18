2165a2bf03aff0380ac4cbba0d3dfbc1637e36ec
Konstantin Kharitonov <hariton70@gmail.com>
2023-04-18 08:57:32 +0500

    2023.04.18

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,17 +1,17 @@
-const main = document.querySelector('#modal_main')
-const success = document.querySelector('.show-success')
-const main_good = document.querySelector('#modal_success')
-const close_x = document.querySelectorAll('.modal__close_times')
+const main = document.querySelector("#modal_main")
+const success = document.querySelector(".show-success")
+const mainGood = document.querySelector("#modal_success")
+const closeX = document.querySelectorAll(".modal__close_times")
 
 main.classList = "modal modal_active"
 
 success.onclick = () => {
-    main_good.classList = "modal modal_active"    
+    mainGood.classList = "modal modal_active"    
 }
 
-close_x.forEach( x => {
+closeX.forEach( x => {
     x.onclick = () => {
         main.classList = "modal"
-        main_good.classList = "modal"    
+        mainGood.classList = "modal"    
     }
 })
